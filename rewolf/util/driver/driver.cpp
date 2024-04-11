3879b9cb9887d97651c8accbb2a81b8fe345b8ab
Verbum <static.faded.cpp@gmail.com>
2024-04-11 18:15:44 -0400

    push

--- rewolf/util/driver/driver.cpp
+++ rewolf/util/driver/driver.cpp
@@ -144,11 +144,11 @@ namespace driver_util {
 
         std::uint8_t buf[hook_sz]{};
         std::memcpy(buf, this->original_instr, hook_sz);
-		const auto res = write_phys_mem<hook_sz>(this->hk_addr, buf);
+        const auto res = write_phys_mem<hook_sz>(this->hk_addr, buf);
 
         delete[] this->original_instr;
         if (!res)
-           return util::log("Failed to unhook NT procedure"), false;
+            return util::log("Failed to unhook NT procedure"), false;
         util::log("Unhooked native procedure");
         return true;
 
