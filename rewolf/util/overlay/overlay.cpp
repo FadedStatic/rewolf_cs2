16600307ca5afffc1882f7fe6921f9cae4ed5913
Verbum <static.faded.cpp@gmail.com>
2024-04-17 23:49:43 -0400

    this is so confusing for no reason at all

--- rewolf/util/overlay/overlay.cpp
+++ rewolf/util/overlay/overlay.cpp
@@ -15,8 +15,50 @@ namespace overlay_util
 		}
 		return { 0, 0 };
 	}
+
+	void init_imgui()
+	{
+		if (SUCCEEDED(swap_chain_ptr->GetDevice(__uuidof(ID3D11Device), (void**)&d3d_device_ptr))) {
+			d3d_device_ptr->GetImmediateContext(&d3d_device_ctx_ptr);
+		}
+		ID3D11Texture2D* RenderTargetTexture = nullptr;
+		if (SUCCEEDED(swap_chain_ptr->GetBuffer(0, IID_PPV_ARGS(&RenderTargetTexture)))) {
+			d3d_device_ptr->CreateRenderTargetView(RenderTargetTexture, NULL, &render_target_view_ptr);
+			if (!render_target_view_ptr)
+				return;
+			RenderTargetTexture->Release();
+		}
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGui_ImplWin32_Init(FindWindow(L"Progman", L"Program Manager"));
+		ImGui_ImplDX11_Init(d3d_device_ptr, d3d_device_ctx_ptr);
+		ImGui::StyleColorsLight();
+		ImGuiIO& io = ImGui::GetIO();
+		io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
+	}
+
+	void draw_overlay(IDXGISwapChain* swap_chain) {
+		swap_chain_ptr = swap_chain;
+
+		// i have NO IDEA why this works, but it does. do not touch this unless you are prepared for the literal shit storm that comes
+		util::log("WHY THE FUCK DOES THIS WORK??");
+		std::call_once(is_initialized, [] {init_imgui();});
+		ImGui_ImplDX11_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+		ImGui::Begin("Hello, world!");
+		ImGui::Text("Hi mom!");
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
+		d3d_device_ctx_ptr->OMSetRenderTargets(1, &render_target_view_ptr, NULL);
+		ImGui::Render();
+		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+		ImGui::EndFrame();
+	}
+
 	long long presentmpo_new(void* thisptr, IDXGISwapChain* a2, __int64 a3, char a4)
 	{
+		draw_overlay(a2);
 		return presentmpo_orig(thisptr, a2, a3, a4);
 	}
 
--- rewolf/util/overlay/overlay.hpp
+++ rewolf/util/overlay/overlay.hpp
@@ -2,6 +2,9 @@
 
 #include "../../libs/scanner/scanner.hpp"
 #include "../../libs/minhook/MinHook.hpp"
+#include "../../libs/imgui/imgui_impl_win32.h"
+#include "../../libs/imgui/imgui.h"
+#include "../../libs/imgui/imgui_impl_dx11.h"
 
 namespace overlay_util
 {
@@ -10,17 +13,25 @@ namespace overlay_util
 	DECLSPEC_SELECTANY extern presentmpo_ty presentmpo_orig{};
 	__int64 __fastcall presentmpo_new(void* thisptr, IDXGISwapChain* a2, __int64 a3, char a4);
 
+	static std::once_flag is_initialized;
+	static ID3D11Device* d3d_device_ptr{};
+	static ID3D11DeviceContext* d3d_device_ctx_ptr{};
+	static IDXGISwapChain* swap_chain_ptr{};
+	static ID3D11RenderTargetView* render_target_view_ptr{};
+
+
 	struct overlay
 	{
-		bool is_initialized{ false };
 		// first is MPO, second is DWM
-		std::pair < std::uintptr_t, std::uintptr_t> find_mpo_and_dwm();
+		static std::pair < std::uintptr_t, std::uintptr_t> find_mpo_and_dwm();
+		void init_imgui();
+		void draw_overlay(IDXGISwapChain* swap_chain);
 
 		overlay() noexcept{
 			const auto [first, second] = find_mpo_and_dwm();
 			if (first && second)
 			{
-				util::log("Initialized hook!");
+				util::log("Hooked...");
 				MH_Initialize();
 				MH_CreateHook(reinterpret_cast<void*>(first), presentmpo_new, reinterpret_cast<LPVOID*>(&presentmpo_orig));
 				MH_EnableHook(nullptr);
